7f27c831c10ad60204564a13e76ba3453baa2c94
Matata <matjanikow@gmail.com>
2024-05-14 15:15:30 -0300

    Agrego camaras

---
+++ Camera2D.gd
@@ -0,0 +1,8 @@
+extends Camera2D
+
+@onready var character = $"../Cubo"
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var cubo_y = character.global_position.y
+	if character.global_position.y < global_position.y:
+		global_position.y = cubo_y
